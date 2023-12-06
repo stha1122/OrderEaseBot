12213e1b74d2f6425cba4f717b5a788a9169b64b
stha <shwethaskumar015@gmail.com>
2023-12-06 23:14:13 +0530

    function get_next_order_id is defined

--- db_helper.py
+++ db_helper.py
@@ -9,6 +9,26 @@ cnx = mysql.connector.connect(
 
 )
 
+# Function to get the next available order_id
+def get_next_order_id():
+    cursor = cnx.cursor()
+
+    # Executing the SQL query to get the next available order_id
+    query = "SELECT MAX(order_id) FROM orders"
+    cursor.execute(query)
+
+    # Fetching the result
+    result = cursor.fetchone()[0]
+
+    # Closing the cursor
+    cursor.close()
+
+    # Returning the next available order_id
+    if result is None:
+        return 1
+    else:
+        return result + 1
+
 # Function to fetch the order status from the order_tracking table
 def get_order_status(order_id):
     cursor = cnx.cursor()
