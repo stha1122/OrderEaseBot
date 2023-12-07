c6f5e120639d1f690e924239d711c290a1be4231
stha <shwethaskumar015@gmail.com>
2023-12-07 13:13:34 +0530

    save_to_db,remove_from_order functions are added

--- db_helper.py
+++ db_helper.py
@@ -9,6 +9,68 @@ cnx = mysql.connector.connect(
 
 )
 
+# Function to call the MySQL stored procedure and insert an order item
+def insert_order_item(food_item, quantity, order_id):
+    try:
+        cursor = cnx.cursor()
+
+        # Calling the stored procedure
+        cursor.callproc('insert_order_item', (food_item, quantity, order_id))
+
+        # Committing the changes
+        cnx.commit()
+
+        # Closing the cursor
+        cursor.close()
+
+        print("Order item inserted successfully!")
+
+        return 1
+
+    except mysql.connector.Error as err:
+        print(f"Error inserting order item: {err}")
+
+        # Rollback changes if necessary
+        cnx.rollback()
+
+        return -1
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        # Rollback changes if necessary
+        cnx.rollback()
+
+        return -1
+
+def get_total_order_price(order_id):
+    cursor = cnx.cursor()
+
+    # Executing the SQL query to get the total order price
+    query = f"SELECT get_total_order_price({order_id})"
+    cursor.execute(query)
+
+    # Fetching the result
+    result = cursor.fetchone()[0]
+
+    # Closing the cursor
+    cursor.close()
+
+    return result
+
+# Function to insert a record into the order_tracking table
+def insert_order_tracking(order_id, status):
+    cursor = cnx.cursor()
+
+    # Inserting the record into the order_tracking table
+    insert_query = "INSERT INTO order_tracking (order_id, status) VALUES (%s, %s)"
+    cursor.execute(insert_query, (order_id, status))
+
+    # Committing the changes
+    cnx.commit()
+
+    # Closing the cursor
+    cursor.close()
+
 # Function to get the next available order_id
 def get_next_order_id():
     cursor = cnx.cursor()
---
+++ generic_helper.py
@@ -0,0 +1,17 @@
+import re
+
+def get_str_from_food_dict(food_dict: dict):
+    result = ", ".join([f"{int(value)} {key}" for key, value in food_dict.items()])
+    return result
+
+
+def extract_session_id(session_str: str):
+    match = re.search(r"/sessions/(.*?)/contexts/", session_str)
+    if match:
+        extracted_string = match.group(1)
+        return extracted_string
+
+    return ""
+
+if __name__ == "__main__":
+    print(get_str_from_food_dict({"samosa":2,"mango lassi":1}))
